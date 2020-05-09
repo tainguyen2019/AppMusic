bee34d7216be193bce21c3a6046f62ac8eea1ad3
Nguyen Tan Tai <tainguyen.uit@gmail.com>
2020-05-09 13:31:05 +0700

    Add Banner to Home Tab

---
+++ app/src/main/java/com/tainguyen/uit/appmusic/Adapter/BannerAdapter.java
@@ -0,0 +1,66 @@
+package com.tainguyen.uit.appmusic.Adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.squareup.picasso.Picasso;
+import com.tainguyen.uit.appmusic.Model.QuangCao;
+import com.tainguyen.uit.appmusic.R;
+
+import java.util.ArrayList;
+
+public class BannerAdapter extends PagerAdapter {
+
+    Context context;
+    ArrayList<QuangCao> arrayListbanner;
+
+    public BannerAdapter(Context context, ArrayList<QuangCao> arrayListbanner) {
+        this.context = context;
+        this.arrayListbanner = arrayListbanner;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayListbanner.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.row_banner, null);
+
+        ImageView imageViewBackgroundBanner = view.findViewById(R.id.imageViewBackgroundBanner);
+        ImageView imageViewSongBanner = view.findViewById(R.id.imageViewSongBanner);
+        TextView textViewTitleSongBanner = view.findViewById(R.id.textViewTitleSongBanner);
+        TextView textViewContentBanner = view.findViewById(R.id.textViewContenBanner);
+
+        Picasso.with(context).load(arrayListbanner.get(position).getHinhAnh()).into(imageViewBackgroundBanner);
+        Picasso.with(context).load(arrayListbanner.get(position).getHinhBaiHat()).into(imageViewSongBanner);
+        textViewTitleSongBanner.setText(arrayListbanner.get(position).getTenBaiHat());
+        textViewContentBanner.setText(arrayListbanner.get(position).getNoiDung());
+
+        container.addView(view);
+
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/tainguyen/uit/appmusic/Fragment/FragmentBanner.java
+++ app/src/main/java/com/tainguyen/uit/appmusic/Fragment/FragmentBanner.java
@@ -1,6 +1,7 @@
 package com.tainguyen.uit.appmusic.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.tainguyen.uit.appmusic.Adapter.BannerAdapter;
 import com.tainguyen.uit.appmusic.Model.QuangCao;
 import com.tainguyen.uit.appmusic.R;
 import com.tainguyen.uit.appmusic.Service.APIService;
@@ -19,20 +22,33 @@ import com.tainguyen.uit.appmusic.Service.IDataService;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class FragmentBanner extends Fragment {
     View view;
+    ViewPager viewPager;
+    CircleIndicator circleIndicator;
+    BannerAdapter bannerAdapter;
+    Runnable runnable;
+    Handler handler;
+    int currentItem;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_banner, container, false);
+        AnhXa();
         getData();
 
         return view;
+    }
+
+    private void AnhXa() {
+        viewPager = view.findViewById(R.id.viewpager);
+        circleIndicator = view.findViewById(R.id.indicatordefault);
     }
 
     private void getData(){
@@ -43,14 +59,33 @@ public class FragmentBanner extends Fragment {
             @Override
             public void onResponse(Call<List<QuangCao>> call, Response<List<QuangCao>> response) {
                 ArrayList<QuangCao> banners = (ArrayList<QuangCao>) response.body();
-                Log.d("BBB",banners.get(0).getTenBaiHat());
+                bannerAdapter = new BannerAdapter(getActivity(), banners);
+                viewPager.setAdapter(bannerAdapter);
+                circleIndicator.setViewPager(viewPager);
+
+                handler = new Handler();
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        currentItem = viewPager.getCurrentItem();
+                        currentItem++;
+                        if(currentItem >= viewPager.getAdapter().getCount()){
+                            currentItem = 0;
+                        }
+
+                        viewPager.setCurrentItem(currentItem, true);
+                        handler.postDelayed(runnable, 4000);
+                    }
+                };
+                handler.postDelayed(runnable, 4500);
 
             }
 
             @Override
             public void onFailure(Call<List<QuangCao>> call, Throwable t) {
-                Log.d("BBB", "Error");
+                Log.d("BBB", t.getMessage());
             }
+
         });
 
     }
